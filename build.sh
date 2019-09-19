37158e20ff8655412103dd227cfc08080545a801
Adam Gąsowski <adam.gasowski@gmail.com>
2019-09-19 15:17:07 +0200

    update build.sh

--- build.sh
+++ build.sh
@@ -2,8 +2,6 @@
 
 VERSIONS=( "5.6" "7.0" "7.1" "7.2" "7.3" "7.4-rc" )
 
-docker login -u gander
-
 NAME="gander/dev"
 
 for VER in "${VERSIONS[@]}"
@@ -14,6 +12,6 @@ do
     docker build -t "${TAG}" -f "${SRC}" . && docker push "${TAG}"
 done
 
-docker tag "${NAME}:7.3" "${NAME}:latest" && docker push "${NAME}:latest"
-docker tag "${NAME}:7.3" "${NAME}:7" && docker push "${NAME}:7"
+docker tag "${NAME}:${VER}" "${NAME}:latest" && docker push "${NAME}:latest"
+docker tag "${NAME}:${VER}" "${NAME}:7" && docker push "${NAME}:7"
 docker tag "${NAME}:5.6" "${NAME}:5" && docker push "${NAME}:5"
