335c2a2dc6102ea1d00ea26bd7eab0eeb596eedf
Adam Gąsowski <adam.gasowski@gmail.com>
2020-01-08 15:26:07 +0100

    Install Symfony CA

--- build.sh
+++ build.sh
@@ -1,19 +1,24 @@
 #!/usr/bin/env bash
 
-VERSIONS=( "5.6" "7.0" "7.1" "7.2" "7.3" "7.4" )
+VERSIONS=("5.6" "7.0" "7.1" "7.2" "7.3" "7.4")
 
 NAME="gander/dev"
 
-for VER in "${VERSIONS[@]}"
-do
-    TAG="${NAME}:${VER}"
-    SRC="Dockerfile.${VER/./}"
+for VER in "${VERSIONS[@]}"; do
+  TAG="${NAME}:${VER}"
+  SRC="Dockerfile.${VER/./}"
 
-    docker build -t "${TAG}" -f "${SRC}" . && docker push "${TAG}"
+  docker build -t "${TAG}" -f "${SRC}" .
+  docker push "${TAG}"
 done
 
-docker tag "${NAME}:${VER}" "${NAME}:latest" && docker push "${NAME}:latest"
-docker tag "${NAME}:${VER}" "${NAME}:7" && docker push "${NAME}:7"
-docker tag "${NAME}:5.6" "${NAME}:5" && docker push "${NAME}:5"
+docker tag "${NAME}:${VER}" "${NAME}:latest"
+docker push "${NAME}:latest"
+
+docker tag "${NAME}:${VER}" "${NAME}:7"
+docker push "${NAME}:7"
+
+docker tag "${NAME}:5.6" "${NAME}:5"
+docker push "${NAME}:5"
 
 echo "Done"
--- rootfs/usr/local/bin/docker-install-symfony.sh
+++ rootfs/usr/local/bin/docker-install-symfony.sh
@@ -1,2 +1,3 @@
 wget https://get.symfony.com/cli/installer -O - | bash
 mv /root/.symfony/bin/symfony /usr/local/bin/symfony
+/usr/local/bin/symfony server:ca:install
