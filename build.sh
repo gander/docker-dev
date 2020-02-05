aa951ba23866249128a65df32b52a96e4228744d
Adam Gąsowski <adam.gasowski@gander.pl>
2020-02-06 00:05:17 +0100

    rebuild

--- build.sh
+++ build.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/env bash
 
+image_build()
+{
+  echo
+  echo "################################   BUILD   ${1}   ################################"
+  echo
+
+  docker build -t "${1}" -f "${2}" .
+}
+
+image_push()
+{
+  echo
+  echo "################################   PUSH   ${1}   ################################"
+  echo
+
+  docker push "${1}"
+}
+
 VERSIONS=("5.6" "7.0" "7.1" "7.2" "7.3" "7.4")
 
 NAME="gander/dev"
@@ -8,17 +26,17 @@ for VER in "${VERSIONS[@]}"; do
   TAG="${NAME}:${VER}"
   SRC="Dockerfile.${VER/./}"
 
-  docker build -t "${TAG}" -f "${SRC}" .
-  docker push "${TAG}"
+  image_build "${TAG}" "${SRC}"
+  image_push "${TAG}"
 done
 
-docker tag "${NAME}:${VER}" "${NAME}:latest"
-docker push "${NAME}:latest"
+docker tag "${TAG}" "${NAME}:latest"
+image_push "${NAME}:latest"
 
-docker tag "${NAME}:${VER}" "${NAME}:7"
-docker push "${NAME}:7"
+docker tag "${TAG}" "${NAME}:7"
+image_push "${NAME}:7"
 
 docker tag "${NAME}:5.6" "${NAME}:5"
-docker push "${NAME}:5"
+image_push "${NAME}:5"
 
 echo "Done"
