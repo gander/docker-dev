764324a3c0550d4dae25b26e73e2cedf83176c95
Adam Gąsowski <adam.gasowski@gander.pl>
2020-02-05 16:53:11 +0100

    progress

--- rootfs/docker-build/50-composer.sh
+++ rootfs/docker-build/50-composer.sh
@@ -4,6 +4,7 @@ EXPECTED_SIGNATURE="$(wget -q -O - https://composer.github.io/installer.sig)"
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 ACTUAL_SIGNATURE="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
 
+# shellcheck disable=SC2039
 if [[ "${EXPECTED_SIGNATURE}" != "${ACTUAL_SIGNATURE}" ]]
 then
     >&2 echo 'ERROR: Invalid installer signature'
