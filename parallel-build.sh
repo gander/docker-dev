f4fe9614aab12b8ffcecd52a37e397e80f0932cd
Adam Gąsowski <adam.gasowski@gander.pl>
2020-07-07 22:16:43 +0200

    update parallel build script

--- parallel-build.sh
+++ parallel-build.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-VERSIONS=("7.4" "7.3" "7.2" "7.1" "7.0" "5.6")
+VERSIONS=("8.0-rc" "7.4" "7.3" "7.2" "7.1" "7.0" "5.6")
 
 image_create() {
   if [ ! ${#} -eq 1 ]; then
     exit 1
   fi
 
-  screen -m -d ./build.sh "${1}"
+  screen -S "docker-build-${1}" -m -d ./build.sh "${1}"
 }
 
 if [ ${#} -gt 0 ]; then
