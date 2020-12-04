a7b9a7bdcf678f771cc886a9ff2591904a4ca77c
Adam Gąsowski <adam.gasowski@gander.pl>
2020-12-04 14:46:43 +0100

    add check.sh

---
+++ check.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+CMD="php -r 'echo phpversion().PHP_EOL;';\
+test -x /usr/local/bin/composer1 && composer1 --version --ansi;\
+test -x /usr/local/bin/composer2 && composer2 --version --ansi;\
+test -x /usr/local/bin/symfony && /usr/local/bin/symfony version --ansi;\
+test -x ~/.composer/vendor/bin/laravel && ~/.composer/vendor/bin/laravel --version --ansi;\
+test -x /usr/local/bin/phpunit && /usr/local/bin/phpunit --version"
+
+for ver in 5.6 7.0 7.1 7.2 7.3 7.4 8.0; do
+  docker run --rm --name "dev-check-${ver}" "gander/dev:${ver}" bash -c "${CMD}"
+done
