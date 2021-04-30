d2c8e5a53abdf38aa5d6814ccb3f48258956e57a
Adam Gąsowski <adam.gasowski@gander.pl>
2021-04-30 08:29:24 +0200

    Fix 99-summary.sh

--- rootfs/docker-build/99-summary.sh
+++ rootfs/docker-build/99-summary.sh
@@ -6,10 +6,10 @@ commands=(
   "php -r \"echo 'PHP:      '.phpversion();\""
   "php -r \"echo 'Zend:     '.zend_version();\""
   "php -r \"echo 'Xdebug:   '.phpversion('xdebug');\""
-  "(test -x /usr/local/bin/composer1 && composer1 --version --ansi || true)"
-  "(test -x /usr/local/bin/composer2 && composer2 --version --ansi || true)"
-  "(test -x /usr/local/bin/symfony && /usr/local/bin/symfony version --ansi || true)"
-  "(test -x ~/.composer/vendor/bin/laravel && ~/.composer/vendor/bin/laravel --version --ansi || true)"
+  "(test -x /usr/local/bin/composer1 && composer1 --version --no-ansi || true)"
+  "(test -x /usr/local/bin/composer2 && composer2 --version --no-ansi || true)"
+  "(test -x /usr/local/bin/symfony && /usr/local/bin/symfony version --no-ansi || true)"
+  "(test -x ~/.composer/vendor/bin/laravel && ~/.composer/vendor/bin/laravel --version --no-ansi || true)"
   "(test -x /usr/local/bin/phpunit9 && /usr/local/bin/phpunit9 --version || true)"
   "(test -x /usr/local/bin/phpunit8 && /usr/local/bin/phpunit8 --version || true)"
   "(test -x /usr/local/bin/phpunit7 && /usr/local/bin/phpunit7 --version || true)"
@@ -20,6 +20,7 @@ commands=(
 
 echo
 echo
+echo "#############################   SUMMARY   #############################"
 echo
 for command in ${!commands[*]}; do
   result=$(bash -c "${commands[$command]}" | xargs)
@@ -30,5 +31,6 @@ for command in ${!commands[*]}; do
 
 done
 echo
+echo "#######################################################################"
 echo
 echo
