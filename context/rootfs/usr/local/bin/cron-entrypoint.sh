17b0058a8a15abecbd7ef8110ed67611c4f39284
Adam Gąsowski <adam.gasowski@gander.pl>
2021-11-29 00:55:46 +0100

    add cron

---
+++ context/rootfs/usr/local/bin/cron-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+printenv | cat - >/tmp/crontab
+
+if [ -f /www/crontab ]; then
+  cat /www/crontab >>/tmp/crontab
+fi
+
+crontab /tmp/crontab
+
+# cron service with SIGTERM support
+sudo service cron start
+sudo trap "service cron stop; exit" SIGINT SIGTERM
+
+# just dump your logs to std output
+tail -f /dev/null &
+wait $!
