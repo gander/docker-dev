85aa2fcb110ea47b7b1289dbbe7dfd212a08bbc9
Adam Gąsowski <adam.gasowski@gander.pl>
2021-12-29 01:42:11 +0100

    composer global install bamarni/composer-bin-plugin; install global packages in their own ns.

---
+++ context/rootfs/docker-build/composer-global-bin.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+if [ ${#} -gt 1 ]; then
+  ns=${1}
+  shift
+
+  for pak in "$@"; do
+    composer global bin "${ns}" require "${pak}"
+  done
+fi
+
+
