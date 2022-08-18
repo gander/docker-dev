07634d177df0ea4e14a73f1773b8e57eda837858
Adam Gąsowski <gander@users.noreply.github.com>
2022-08-18 08:15:07 +0200

    Update 60-symfony.sh

--- rootfs/setup/60-symfony.sh
+++ rootfs/setup/60-symfony.sh
@@ -3,5 +3,5 @@ set -euo pipefail
 IFS=$'\n\t'
 
 wget https://get.symfony.com/cli/installer -O - | bash &&
-  mv /root/.symfony/bin/symfony /usr/local/bin/symfony &&
+  mv /root/.symfony5/bin/symfony /usr/local/bin/symfony &&
   mkdir -p ~dev/.symfony && chown dev:dev ~dev/.symfony
