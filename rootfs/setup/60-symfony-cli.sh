f630a21efdcdc0bbb9ec76789cd02876b96416f3
Adam Gąsowski <gander@users.noreply.github.com>
2022-08-19 21:29:08 +0200

    Update 60-symfony-cli.sh

--- rootfs/setup/60-symfony-cli.sh
+++ rootfs/setup/60-symfony-cli.sh
@@ -2,6 +2,5 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-echo 'deb [trusted=yes] https://repo.symfony.com/apt/ /' | tee /etc/apt/sources.list.d/symfony-cli.list &&
-  apt update &&
-  apt install symfony-cli
+curl -1sLf 'https://dl.cloudsmith.io/public/symfony/stable/setup.deb.sh' | bash
+apt install symfony-cli
