32fdb3f866120e216c0a7cb63db714583db1f7dd
Adam Gąsowski <adam.gasowski@gander.pl>
2023-01-22 11:09:10 +0100

    retry curl bugfix

--- rootfs/setup/90-utils.sh
+++ rootfs/setup/90-utils.sh
@@ -2,14 +2,14 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-RELEASE=$(curl --retry-all-errors --retry 3 https://api.github.com/repos/fabpot/local-php-security-checker/releases/latest | jq --raw-output '.assets[]?|.browser_download_url|select(endswith("_linux_amd64"))')
+RELEASE=$(curl --retry-connrefused --retry 3 https://api.github.com/repos/fabpot/local-php-security-checker/releases/latest | jq --raw-output '.assets[]?|.browser_download_url|select(endswith("_linux_amd64"))')
 
 if [ -n "${RELEASE}" ]; then
-  curl --retry-all-errors --retry 3 -LsS "${RELEASE}" -o "/usr/local/bin/local-php-security-checker"
+  curl --retry-connrefused --retry 3 -LsS "${RELEASE}" -o "/usr/local/bin/local-php-security-checker"
   chmod +x "/usr/local/bin/local-php-security-checker"
 fi
 
 if [[ ! $(php -r 'echo PHP_VERSION;') < '7.4.0' ]]; then
-  curl -LsS https://github.com/composer-unused/composer-unused/releases/latest/download/composer-unused.phar -o /usr/local/bin/composer-unused
+  curl --retry-connrefused --retry 3 -LsS https://github.com/composer-unused/composer-unused/releases/latest/download/composer-unused.phar -o /usr/local/bin/composer-unused
   chmod +x /usr/local/bin/composer-unused
 fi
