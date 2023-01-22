a8a89fd67399f3195096a00d52b19edbb5344745
Adam Gąsowski <adam.gasowski@gander.pl>
2023-01-22 11:03:06 +0100

    retry curl

--- rootfs/setup/90-utils.sh
+++ rootfs/setup/90-utils.sh
@@ -5,7 +5,7 @@ IFS=$'\n\t'
 RELEASE=$(curl https://api.github.com/repos/fabpot/local-php-security-checker/releases/latest | jq --raw-output '.assets[]?|.browser_download_url|select(endswith("_linux_amd64"))')
 
 if [ -n "${RELEASE}" ]; then
-  curl -LsS "${RELEASE}" -o "/usr/local/bin/local-php-security-checker"
+  curl --retry-all-errors --retry 3 -LsS "${RELEASE}" -o "/usr/local/bin/local-php-security-checker"
   chmod +x "/usr/local/bin/local-php-security-checker"
 fi
 
