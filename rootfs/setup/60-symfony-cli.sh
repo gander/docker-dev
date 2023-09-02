72d84802f79900bec95418045b10dc19442de36a
Adam Gąsowski <adam.gasowski@gander.pl>
2023-09-02 14:28:40 +0200

    fix: Symfony CLI install process

--- rootfs/setup/60-symfony-cli.sh
+++ rootfs/setup/60-symfony-cli.sh
@@ -4,4 +4,6 @@ IFS=$'\n\t'
 
 wget https://get.symfony.com/cli/installer -O - | bash
 
-/usr/bin/symfony version --no-ansi >> /var/www/html/versions.txt
+mv /root/.symfony5/bin/symfony /usr/local/bin/symfony
+
+/usr/local/bin/symfony version --no-ansi >> /var/www/html/versions.txt
